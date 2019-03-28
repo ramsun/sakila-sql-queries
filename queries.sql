9e3fce8aed7abe23c2eba54c8cfbcbffb31bff9e
Ramamurthy Sundar <rsramamurthysundar@gmail.com>
2019-03-28 00:27:06 -0400

    Finished joins and basic subqueries

--- queries.sql
+++ queries.sql
@@ -9,7 +9,7 @@ FROM actor;
 SELECT CONCAT(first_name,' ',last_name) AS 'Actor Name' 
 FROM actor;
 
--- 1b
+-- Diplay information for the actor with the first name Joe
 SELECT actor_id, first_name, last_name 
 FROM actor
 WHERE first_name = 'Joe';
@@ -65,13 +65,77 @@ UPDATE actor
 SET first_name = 'GROUCHO', last_name = 'WILLIAMS'  
 WHERE (first_name = 'HARPO' AND last_name = 'WILLIAMS');
 
--- 5a - Recreate the table within the 
+-- 5a - Recreate the table address 
 CREATE TABLE IF NOT EXISTS address (
-
 address_id INT AUTO_INCREMENT,
-address VARCHAR(255) NOT NULL,
-address2 VARCHAR(255) NOT NULL,
-district VARCHAR(20) NOT NULL,
-city_id INT AUTO_INCREMENT NOT NULL,
-
+address VARCHAR(50),
+address2 VARCHAR(50),
+district VARCHAR(20),
+city_id INT AUTO_INCREMENT,
+postal_code VARCHAR(10) NOT NULL,
+phone VARCHAR(20) NOT NULL,
+last_update TIMESTAMP NOT NULL
 )
+
+-- 6a
+SELECT first_name, last_name, address
+FROM address
+INNER JOIN staff ON staff.address_id = address.address_id;
+
+-- 6b
+SELECT first_name, last_name, amount
+FROM staff
+INNER JOIN payment ON staff.staff_id = payment.staff_id;
+
+-- 6c
+SELECT title, count(actor_id)
+FROM film
+INNER JOIN film_actor ON film.film_id = film_actor.film_id
+GROUP BY title;
+
+-- 6d
+SELECT title, count(inventory_id)
+FROM film
+INNER JOIN inventory ON film.film_id = inventory.film_id
+WHERE title = "Hunchback Impossible"
+GROUP BY title;
+
+-- 6e
+SELECT title, count(inventory_id)
+FROM film
+INNER JOIN inventory ON film.film_id = inventory.film_id
+WHERE title = "Hunchback Impossible"
+GROUP BY title;
+
+-- 6e
+SELECT first_name, last_name, sum(amount)
+FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id
+GROUP BY last_name;
+
+-- 7a
+SELECT title
+FROM film
+WHERE title LIKE 'K%' OR title LIKE 'Q%'
+AND language_id IN(	SELECT language_id
+					FROM language
+					WHERE name = 'English');
+
+-- 7b
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id IN(SELECT actor_id
+				FROM film
+				WHERE title = 'Alone Trip');
+                
+-- 7c
+
+	
+
+
+
+
+
+
+
+
